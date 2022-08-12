a68c4b5aad70a770e9c895b2383886e00a553773
saw thandar <sawthandar1998.pw@gmail.com>
2022-08-12 16:47:21 +0630

    need to fix product_price_list

--- app/src/main/java/com/busybees/lauk_kaing_expert_services/activity/PaymentActivity.java
+++ app/src/main/java/com/busybees/lauk_kaing_expert_services/activity/PaymentActivity.java
@@ -40,11 +40,18 @@ import com.busybees.lauk_kaing_expert_services.utility.ApiConstants;
 import com.busybees.lauk_kaing_expert_services.utility.AppENUM;
 import com.busybees.lauk_kaing_expert_services.utility.AppStorePreferences;
 import com.busybees.lauk_kaing_expert_services.utility.Utility;
+import com.google.android.exoplayer2.util.Util;
 
 import org.greenrobot.eventbus.EventBus;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 
 import me.myatminsoe.mdetect.MDetect;
@@ -62,16 +69,16 @@ public class PaymentActivity extends AppCompatActivity {
     private PaymentAdapter adapter;
     private LinearLayoutManager paymentLayoutManager;
 
-    private LinearLayout continueLayout, viewSubTotal, viewDiscount, viewTotal;
+    private LinearLayout continueLayout, viewSubTotal, viewDiscount, viewTotal, viewPromoCode;
 
     private ProgressBar progressBar;
-    private TextView subtotal, discountTotal, total;
+    private TextView subtotal, discountTotal, total, promo_discount;
 
     private ImageView back, homePageView, cartPageView, addressPageView, finalOrderPageView;
 
     ArrayList<GetCartDataModel> cartDatas = new ArrayList<>();
     ArrayList<PromoCodeVO> promoCodeVOArrayList = new ArrayList<>();
-
+    List<ProductPriceListVO> p;
     private String serviceName;
 
     private ArrayList<ProductPriceListVO> productPriceListVOArrayList = new ArrayList<>();
@@ -101,10 +108,13 @@ public class PaymentActivity extends AppCompatActivity {
         viewSubTotal = findViewById(R.id.sub_total_view);
         viewDiscount = findViewById(R.id.discount_view);
         viewTotal = findViewById(R.id.total_view);
+        viewPromoCode = findViewById(R.id.promo_discount_view);
+        promo_discount = findViewById(R.id.promo_amount);
 
         setUpAdapter();
         onClick();
         CallGetCart();
+
     }
 
     private void onClick() {
@@ -132,6 +142,7 @@ public class PaymentActivity extends AppCompatActivity {
                 for (int i = 0; i < promoCodeVOArrayList.size(); i++) {
                     if (promoCodeVOArrayList.get(i).getPromoActive() == 1) {
                         list.add(promoCodeVOArrayList.get(i).getServiceName());
+                    } else {
                     }
                 }
 
@@ -181,32 +192,19 @@ public class PaymentActivity extends AppCompatActivity {
                     }
                 });
 
+
                 promoCodeDialogView.findViewById(R.id.promo_code_ok_btn).setOnClickListener(v1 -> {
 
-                    if (!promoCodeName.getText().toString().equalsIgnoreCase("")) {
-                        for (int carts = 0; carts < cartDatas.size(); carts++) {
-                            productPriceListVO.setPriceId(cartDatas.get(carts).getProductPriceId());
-                            productPriceListVO.setItemTotal(cartDatas.get(carts).getAmount());
-                            productPriceListVOArrayList.add(productPriceListVO);
-                        }
+                    matchPromoCodeObject.setPromoCode(promoCodeName.getText().toString());
+                    CallMatchPromoCode(matchPromoCodeObject);
+                    promoCodeDialog.dismiss();
 
-                        matchPromoCodeObject.setProductPriceList(productPriceListVOArrayList);
-                        matchPromoCodeObject.setPhone(userObj.getPhone());
-                        matchPromoCodeObject.setPromoCode(promoCodeName.getText().toString());
-                        CallMatchPromoCode(matchPromoCodeObject);
-
-                    } else {
-
-                    }
                 });
 
                 promoCodeDialogView.findViewById(R.id.promo_cancel_btn).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         promoCodeDialog.dismiss();
-                        /*specificInfo.setPrice_id("");
-                        specificInfo.setVcode("");
-                        sameCode();*/
                     }
                 });
 
@@ -246,8 +244,9 @@ public class PaymentActivity extends AppCompatActivity {
                 @Override
                 public void onResponse(Call<MatchPromoCodeModel> call, Response<MatchPromoCodeModel> response) {
                     if (response.body().isError() == false) {
-                        Utility.showToast(getApplicationContext(), response.body().getMessage());
-
+                        viewPromoCode.setVisibility(View.VISIBLE);
+                        promo_discount.setText("( " + response.body().getData().getDiscount() + " ) " + getString(R.string.currency));
+                        total.setText(response.body().getData().getTotal() + " " + getString(R.string.currency));
                     } else if (response.body().isError() == true) {
                         Utility.showToast(PaymentActivity.this, response.body().getMessage());
                     }
@@ -279,19 +278,32 @@ public class PaymentActivity extends AppCompatActivity {
                         if (response.body().getError() == false) {
                             cartDatas.clear();
                             cartDatas.addAll(response.body().getData());
+
+                            for (int i = 0; i < cartDatas.size(); i++) {
+
+                                productPriceListVO.setPriceId(cartDatas.get(i).getProductPriceId());
+                                productPriceListVO.setItemTotal(cartDatas.get(i).getAmount());
+                                productPriceListVOArrayList.add(productPriceListVO);
+
+                            }
+
+                            matchPromoCodeObject.setProductPriceList(productPriceListVOArrayList);
+                            matchPromoCodeObject.setPhone(userObj.getPhone());
+
                             promoCodeVOArrayList.addAll(response.body().getPromoCodeList());
+
                             adapter = new PaymentAdapter(PaymentActivity.this, cartDatas);
                             paymentRecyclerView.setAdapter(adapter);
                             adapter.notifyDataSetChanged();
 
-                            subtotal.setText(NumberFormat.getNumberInstance(Locale.US).format(response.body().getItemTotal()) + getString(R.string.currency));
+                            subtotal.setText(NumberFormat.getNumberInstance(Locale.US).format(response.body().getItemTotal()) + " " + getString(R.string.currency));
                             if (response.body().getDiscountTotalAll() == 0) {
                                 discountTotal.setText("-");
                             } else if (response.body().getDiscountTotalAll() > 0) {
-                                discountTotal.setText("( " + NumberFormat.getNumberInstance(Locale.US).format(response.body().getDiscountTotalAll()) + " )" + getString(R.string.currency));
+                                discountTotal.setText("( " + NumberFormat.getNumberInstance(Locale.US).format(response.body().getDiscountTotalAll()) + " ) " + getString(R.string.currency));
                             }
 
-                            total.setText(response.body().getTotal() + getString(R.string.currency));
+                            total.setText(response.body().getTotal() + " " + getString(R.string.currency));
 
                         } else {
                             Utility.showToast(getApplicationContext(), response.body().getMessage());
@@ -324,4 +336,5 @@ public class PaymentActivity extends AppCompatActivity {
         getWindow().setStatusBarColor(Color.TRANSPARENT);
 
     }
+
 }
